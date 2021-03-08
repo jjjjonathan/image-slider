275f66e651039260ac100499e2e63b9651cab5ec
jjjjonathan <hornathanjon@gmail.com>
2021-03-07 21:26:08 -0600

    final

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ function updateMargin() {
   document.querySelector(".images").style.marginLeft = margin;
 }
 
+function updateCircle() {
+  const allCircles = document.querySelectorAll(".circle");
+  allCircles.forEach((circle) => {
+    circle.classList.remove("active");
+  });
+
+  const newActiveCircle = document.querySelector(
+    `.circle[data-index="${imageIndex}"]`
+  );
+  newActiveCircle.classList.add("active");
+}
+
 function goLeft() {
   if (imageIndex > 1) {
     imageIndex -= 1;
     updateMargin();
+    updateCircle();
   }
 }
 
@@ -16,19 +29,23 @@ function goRight() {
   if (imageIndex < 8) {
     imageIndex += 1;
     updateMargin();
+    updateCircle();
   }
 }
 
 function handleCircleClick(event) {
-  const circle = event.target.id;
+  const circle = event.target.dataset.index;
 
-  console.log(circle);
+  imageIndex = parseInt(circle);
+
+  updateMargin();
+  updateCircle();
 }
 
 function onload() {
   const leftArrow = document.querySelector(".left-arrow");
   const rightArrow = document.querySelector(".right-arrow");
-  const circles = document.querySelectorAll(".circles");
+  const circles = document.querySelectorAll(".circle");
 
   leftArrow.addEventListener("click", goLeft);
   rightArrow.addEventListener("click", goRight);
